7b0be84486245b8b13d4c77c5dca63061eb1a372
Helena Makendengue <HelenaMakens1013@gmail.com>
2023-03-01 11:43:05 -0600

    Update Assessment1ControllerAdvice.java

--- src/main/java/com/cooksys/assessment1/controllers/advice/Assessment1ControllerAdvice.java
+++ src/main/java/com/cooksys/assessment1/controllers/advice/Assessment1ControllerAdvice.java
@@ -9,9 +9,11 @@ import com.cooksys.assessment1.dtos.ErrorDto;
 import com.cooksys.assessment1.exceptions.BadRequestException;
 
 @ControllerAdvice(basePackages = {"com.cooksys.assessment1.controllers"})
+@ResponseBody
 public class Assessment1ControllerAdvice {
 	
 	@ExceptionHandler(BadRequestException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ErrorDto handleBadREquestException(HttpServletRequest request, BadRequestException badRequestException) {
 		return new ErrorDto(badRequestException.getMessage());
 	}
