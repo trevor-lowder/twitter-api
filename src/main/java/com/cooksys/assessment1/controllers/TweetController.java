fb596d823dbf6ae89b67a493fa93c91bce627dd2
TSL-coding <lowder.trevor@yahoo.com>
2023-03-03 10:27:11 -0700

    remove PatchMaping

--- src/main/java/com/cooksys/assessment1/controllers/TweetController.java
+++ src/main/java/com/cooksys/assessment1/controllers/TweetController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -60,22 +59,16 @@ public class TweetController {
 		return tweetService.getMentions(id);
 	}
 
-	@PatchMapping("/{id}")
-	public TweetResponseDto updateTweet(@PathVariable Long id, @RequestBody TweetRequestDto tweetRequestDto) {
-		// TODO: Implement this method
-		return null;
-	}
-
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public TweetResponseDto deleteTweet(@PathVariable Long id) {
 		TweetResponseDto tweetResponseDto = tweetService.deleteTweet(id);
 		return tweetResponseDto;
 	}
-	
+
 	@GetMapping("/{id}/tags")
 	public List<HashtagDto> getHashtagsFromTweetId(@PathVariable Long id) {
 		return tweetService.getHashtagsFromTweetId(id);
-		
+
 	}
 }
